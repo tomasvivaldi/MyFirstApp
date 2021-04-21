c041de8f79e103c54fba450c1d61f6d19a28712d
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2021-04-21 18:46:02 +0900

    first step of the app

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeButtonClick(_ sender: Any) {
+        
+        imageView.image = UIImage(named: "stormy-night-over-byron-bay-673747736-5c48ab2a46e0fb0001ef2e87")
+    }
+    
 }
 
